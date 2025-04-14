ff02a8c5ad2d256934b24ea8996da383ba68bbc2
gjsoy24 <gour.joy24@gmail.com>
2025-04-14 18:10:32 +0600

    add D_Fixed_Password

---
+++ E_Max.c
@@ -0,0 +1,9 @@
+//! Given a number N, and N numbers, find maximum number in these N numbers.
+
+//* Input
+//* First line contains a number N (1 ≤ N ≤ 103).
+
+//* Second line contains N numbers Xi (0 ≤ Xi ≤ 109).
+
+//* Output
+//* Print the maximum number.
